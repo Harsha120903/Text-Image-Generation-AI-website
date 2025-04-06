5ed449629c955c1aec1f4255a259cc7e04b4ba8a
Harsha120903 <22211a6762@bvrit.ac.in>
2025-04-06 18:38:32 +0530

    change in uri

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,6 +4,6 @@ const connectDB =async()=>{
   mongoose.connection.on('connected',()=>{
    console.log('database connected');
   })
-  await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
+  await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 export default connectDB; 
